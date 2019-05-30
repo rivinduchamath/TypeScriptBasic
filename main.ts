12c6e3bc3105fec789784f26bfe2af6b2e440767
rivinduc <trivinduchamath333@gmail.com>
2019-05-30 16:31:14 +0530

    Basic Type Script

--- main.ts
+++ main.ts
@@ -8,13 +8,13 @@ function e(){
 e();
 
 //function with for loop
-function s(){
+function s1(){
     for (let i = 0; i < 5; i++) {
         console.log(i)    
     }
     // console.log(i) Error: becouse let data type replace var
 }
-function s()
+ s1()
 const ColorR = "Red"
 const ColorG = 'Green'
 
@@ -31,21 +31,24 @@ let f:any[] =['ewe',433,true]
 
 //Enum
 enum color {Red =1,Blue =2}
-let backGround = Color.Red;
+let backGround = color.Red;
 
 //Assertions
 let message; // any type variable so that it might be confuse when assign to another variable 
 message  = 'abc'
-let endsWithc = (<string>message).endsWith('c');//OR let endsWithc = (message as string).endsWith('c') else let message = 'abc'
+let endsWithc = (message).endsWith('c');//OR let endsWithc = (message as string).endsWith('c') else let message = 'abc'
 
 //Arrow Function
 let log = function(message){
 console.log(message)
 }
-let DoLog = () => console.log();
-
+let DoLog = (m) => console.log(m);
+//callto it
+ DoLog({
+   m:545
+ })
 //interface
-interface Point{
+interface Point{ //Encapsul
     x:number,
     y:number
 }
@@ -54,3 +57,18 @@ drawpoint({
     x:1,
     y:2
 })
+
+{
+//class (oop consept Cohesion)
+class Point{
+    x1:number;
+    y1:number;
+
+    draw(){
+
+    }
+    getDistance(another:Point){
+
+    }
+}
+}
